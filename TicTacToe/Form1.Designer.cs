f1ef8aa34c352f5c02eed19f3325b99a9cda2a22
davis0011 <Davis85754556>
2022-05-28 10:54:06 +0300

    Finished working on the mechanizm of the game, can now play a game with winner announced

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -73,72 +73,88 @@
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(3, 396);
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(396, 395);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(192, 192);
             this.button9.TabIndex = 9;
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button8
             // 
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button8.Location = new System.Drawing.Point(198, 396);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(192, 192);
             this.button8.TabIndex = 8;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(396, 396);
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(3, 396);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(192, 192);
             this.button7.TabIndex = 7;
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(3, 198);
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(396, 198);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(192, 192);
             this.button6.TabIndex = 6;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(396, 198);
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(198, 198);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(192, 192);
             this.button5.TabIndex = 5;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(198, 198);
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(3, 198);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(192, 192);
             this.button4.TabIndex = 4;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button3.Location = new System.Drawing.Point(396, 3);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(192, 192);
             this.button3.TabIndex = 3;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(198, 3);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(192, 192);
             this.button1.TabIndex = 2;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Location = new System.Drawing.Point(198, 3);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(192, 192);
             this.button2.TabIndex = 1;
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -2,28 +2,275 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
-        private static char turn;
+        public static char turn;
+        public static char playerturn;
+        public static string[] player1 = new string[2];
+        public static string[] player2 = new string[2];
         public Form1()
         {
             InitializeComponent();
-            Random rd = new Random();
-            int start = rd.Next(2);
-            if (start == 0)
-                turn = 'X';
-            else
-                turn = 'Y';
+            turnpick();
+           
         }
+        void turnpick()
+        {
+            Random rd = new Random();
+            int start = rd.Next(3);
+            if (start == 1)
+            {
+                playerturn = '1';
+                turn = 'X';
+                player1[0] = "1";
+                player1[1] = "X";
+                player2[0] = "2";
+                player2[1] = "O";
 
+            }
+            else
+            {
+                playerturn = '1';
+                turn = 'O';
+                player1[0] = "1";
+                player1[1] = "O";
+                player2[0] = "2";
+                player2[1] = "X";
+            }
+        }
+        void ChangeTurn()
+        {
+            if (turn == 'X')
+                turn = 'O';
+            else
+            {
+                turn = 'X';
+            }
+            if (playerturn == '1')
+                playerturn = '2';
+            else
+                playerturn = '1';
+        }
+        void isWin()
+        {
+            if(button1.Text == button2.Text && button2.Text == button3.Text && button3.Text != "")
+            {
+                    MessageBox.Show("The winner is player" + playerturn + "!");
+            }
+            if (button4.Text == button5.Text && button5.Text == button6.Text && button4.Text != "")
+            {
+                MessageBox.Show("The winner is player" + playerturn + "!");
+            }
+            if (button7.Text == button8.Text && button8.Text == button9.Text && button7.Text != "")
+            {
+                MessageBox.Show("The winner is player" + playerturn + "!");
+            }
+            if (button1.Text == button4.Text && button4.Text == button7.Text && button1.Text != "")
+            {
+                MessageBox.Show("The winner is player" + playerturn + "!");
+            }
+            if (button2.Text == button5.Text && button5.Text == button8.Text && button2.Text != "")
+            {
+                MessageBox.Show("The winner is player" + playerturn + "!");
+            }
+            if (button3.Text == button6.Text && button6.Text == button9.Text && button3.Text != "")
+            {
+                MessageBox.Show("The winner is player" + playerturn + "!");
+            }
+            if (button1.Text == button5.Text && button5.Text == button9.Text && button1.Text != "")
+            {
+                MessageBox.Show("The winner is player" + playerturn + "!");
+            }
+            if (button3.Text == button5.Text && button5.Text == button7.Text && button3.Text != "")
+            {
+                MessageBox.Show("The winner is player" + playerturn + "!");
+            }
+        }
+        void enableButton()
+        {
+        }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text != "")
+            {
+                return;
+            }
+            if (turn == 'X')
+            {
+                button1.Text = "X";
+                isWin();
+                ChangeTurn();
+            }
+            else
+            {
+                button1.Text = "O";
+                isWin();
+                ChangeTurn();
+            }
+            
+        }
         private void button2_Click(object sender, EventArgs e)
         {
-            if(turn == 'X')
+            if (button2.Text != "")
+            {
+                return;
+            }
+            if (turn == 'X')
             {
                 button2.Text = "X";
+                isWin();
+                ChangeTurn();
+            }
+            else
+            {
+                button2.Text = "O";
+                isWin();
+                ChangeTurn();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(button3.Text != "")
+            {
+                return;
+            }
+            if (turn == 'X')
+            {
+                button3.Text = "X";
+                isWin();
+                ChangeTurn();
+            }
+            else
+            {
+                button3.Text = "O";
+                isWin();
+                ChangeTurn();
+            }
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text != "")
+            {
+                return;
+            }
+            if (turn == 'X')
+            {
+                button4.Text = "X";
+                isWin();
+                ChangeTurn();
+            }
+            else
+            {
+                button4.Text = "O";
+                isWin();
+                ChangeTurn();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text != "")
+            {
+                return;
+            }
+            if (turn == 'X')
+            {
+                button5.Text = "X";
+                isWin();
+                ChangeTurn();
+            }
+            else
+            {
+                button5.Text = "O";
+                isWin();
+                ChangeTurn();
+            }
+            isWin();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text != "")
+            {
+                return;
+            }
+            if (turn == 'X')
+            {
+                button6.Text = "X";
+                isWin();
+                ChangeTurn();
+            }
+            else
+            {
+                button6.Text = "O";
+                isWin();
+                ChangeTurn();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.Text != "")
+            {
+                return;
+            }
+            if (turn == 'X')
+            {
+                button7.Text = "X";
+                isWin();
+                ChangeTurn();
+            }
+            else
+            {
+                button7.Text = "O";
+                isWin();
+                ChangeTurn();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.Text != "")
+            {
+                return;
+            }
+            if (turn == 'X')
+            {
+                button8.Text = "X";
+                isWin();
+                ChangeTurn();
+            }
+            else
+            {
+                button8.Text = "O";
+                isWin();
+                ChangeTurn();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.Text != "")
+            {
+                return;
+            }
+            if (turn == 'X')
+            {
+                button9.Text = "X";
+                isWin();
+                ChangeTurn();
+            }
+            else
+            {
+                button9.Text = "O";
+                isWin();
+                ChangeTurn();
             }
         }
     }
